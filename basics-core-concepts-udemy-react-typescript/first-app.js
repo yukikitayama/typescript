3c74c127832d7f818d87fc1649b2a23d33c404f8
yukikitayama <yk2797@columbia.edu>
2024-07-03 13:49:33 -0700

    review basics

---
+++ basics-core-concepts-udemy-react-typescript/first-app.js
@@ -0,0 +1,5 @@
+var userName = 'Max';
+// userName = 34;
+userName = 'Yuki';
+var userAge = 34;
+var isValid = true;
---
+++ basics-core-concepts-udemy-react-typescript/first-app.ts
@@ -0,0 +1,26 @@
+let userName = 'Max';
+
+// userName = 34;
+userName = 'Yuki';
+
+let userAge = 34;
+
+let isValid = true;
+
+let userID: string | number = 'abc';
+userID = 123;
+
+// let user: object;
+let user: {
+  name: string;
+  age: number;
+  isAdmin: boolean;
+  id: string | number;
+};
+
+user = {
+  name: 'Max',
+  age: 34,
+  isAdmin: true,
+  id: 'abc' // or 123
+};
